57facd7727c6a8889de831dd20728dd140e0ab21
tentetmr <keisuke.tamura.osaka@gmail.com>
2021-05-22 23:44:27 +0900

    Initial Commit

---
+++ Scraping/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scraping
+//
+//  Created by 田村圭佑 on 2021/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
